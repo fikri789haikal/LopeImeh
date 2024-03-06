5fadfe63466b3b40a4eb4417bb733d8970709c3b
Muhamad FIkri Haikal <161137262+fikri789haikal@users.noreply.github.com>
2024-03-06 23:28:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["L❤PE Imehh", "Taayang Dde Imehh💖", "micuu❤ imehhh"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
